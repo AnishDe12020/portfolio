90203621c8476532f3635144465291259c76f604
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-10 14:18:30 +0530

    refactor: moved header links to its own reusable component

---
+++ src/components/header-link.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let linkContent;
+  export let linkHref;
+</script>
+
+<li>
+  <a
+    class="bg-accent text-secondary rounded-lg px-4 py-3 font-semibold font-serif hover:opacity-70 size text-xl focus:outline-none focus:ring mx-4"
+    href={linkHref}
+  >
+    {linkContent}
+  </a>
+</li>
--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,22 +1,11 @@
+<script>
+  import HeaderLink from "./header-link.svelte";
+</script>
+
 <div class="bg-secondary w-screen py-8 flex flex-row justify-end pr-32">
   <ul class="flex flex-row">
-    <li>
-      <a
-        class="bg-accent text-secondary rounded-lg px-4 py-3 font-semibold font-serif hover:opacity-70 size text-xl focus:outline-none focus:ring mx-4"
-        href="#">Projects</a
-      >
-    </li>
-    <li>
-      <a
-        class="bg-accent text-secondary rounded-lg px-4 py-3 font-semibold font-serif hover:opacity-70 size text-xl focus:outline-none focus:ring mx-4"
-        href="#">Contact</a
-      >
-    </li>
-    <li>
-      <a
-        class="bg-accent text-secondary rounded-lg px-4 py-3 font-semibold font-serif hover:opacity-70 size text-xl focus:outline-none focus:ring mx-4"
-        href="#">About</a
-      >
-    </li>
+    <HeaderLink linkHref="#" linkContent="Projects" />
+    <HeaderLink linkHref="#" linkContent="Contact" />
+    <HeaderLink linkHref="#" linkContent="About" />
   </ul>
 </div>

7cf3b6aa551a1a2133b4d817f9f0438c19c23e0f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-10 19:15:51 +0530

    chore: added some content and avatar in index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,12 @@
-<h1 class="text-white">Site WIP</h1>
+<div
+  class="flex flex-col items-center justify-center mt-32 md:flex-row md:ml-16 md:justify-start md:items-start"
+>
+  <h1 class="text-white text-4xl">Hey everyone</h1>
+  <div class="realtive mx-auto ">
+    <img
+      src="images/avatar.svg"
+      alt="Avatar"
+      class="inline-block rounded-full h-auto w-64"
+    />
+  </div>
+</div>

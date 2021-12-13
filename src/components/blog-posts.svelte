25116e271ed54ec08b3178b7c1be43c61b1605d4
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-13 12:15:22 +0530

    style: changed box shadow and added hover effects for box shadow on blog posts card

--- src/components/blog-posts.svelte
+++ src/components/blog-posts.svelte
@@ -8,7 +8,9 @@
 >
   {#each blogPosts as post}
     <div class="p-4">
-      <div class="bg-secondary rounded-xl shadow-lg p-4">
+      <div
+        class="bg-secondary rounded-xl shadow-md shadow-secondary p-4 hover:shadow-lg hover:shadow-accent transition duration-150 ease-out"
+      >
         <img src={post.coverImage} alt={post.title} />
         <h2 class="mt-4 text-2xl font-bold text-white text-wra">
           {post.title}

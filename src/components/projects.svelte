2ed0cf62bd109107f0b4837791e01b014b4df2de
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-15 15:22:13 +0530

    style: made projects more responsive

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -4,12 +4,18 @@
 
 <div>
   {#each projects as project}
-    <div class="flex m-8 h-96 w-auto">
-      <img src={project.image} class="rounded-xl" />
-      <div class="flex flex-col justify-between ml-16">
-        <div>
+    <div class="flex flex-col md:flex-row mt-8 mx-8 md:mt-16 md:mx-16">
+      <img src={project.image} class="w-11/12 md:w-1/2 rounded-xl" />
+      <div
+        class="flex flex-col justify-center items-center md:justify-between mt-8 md:mt-0 md:ml-16"
+      >
+        <div
+          class="flex flex-col justify-center items-center md:justify-left md:items-start"
+        >
           <h2 class="text-7xl font-bold text-white">{project.name}</h2>
-          <p class="text-gray-300 font-semibold text-3xl mt-4">
+          <p
+            class="text-gray-300 font-semibold text-3xl mt-8 text-center md:text-left"
+          >
             {project.description}
           </p>
         </div>

9d1b050e2a08d90234625fe3fe8c1cb0784a184f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-16 10:59:58 +0530

    style: centered project image

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -4,7 +4,9 @@
 
 <div>
   {#each projects as project}
-    <div class="flex flex-col md:flex-row mt-8 mx-8 md:mt-16 md:mx-16">
+    <div
+      class="flex flex-col justify-center items-center md:justify-start md:items-start md:flex-row mt-8 mx-8 md:mt-16 md:mx-16"
+    >
       <img
         src={project.image}
         class="w-11/12 md:w-1/2 rounded-xl"

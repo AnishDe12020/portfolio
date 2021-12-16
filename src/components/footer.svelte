72f85d139e36152c3fbcebcbcb885bc486f8252a
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-16 12:12:22 +0530

    feat: added a simple footer

---
+++ src/components/footer.svelte
@@ -0,0 +1,14 @@
+<div class="flex flex-col justify-center items-center mt-16 mb-8 text-white">
+  <p>
+    Made by <a
+      href="https://twitter.com/AnishDe12020"
+      class="text-accent hover:opacity-30">Anish De</a
+    >
+  </p>
+  <p class="mt-4">
+    Check out the <a
+      href="https://github.com/AnishDe12020/portfolio"
+      class="text-accent hover:opacity-30">source code</a
+    >
+  </p>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,10 +2,12 @@
   import "../app.css";
   import WipBanner from "../components/wip-banner.svelte";
   import Header from "../components/header.svelte";
+  import Footer from "../components/footer.svelte";
 </script>
 
 <div>
   <WipBanner />
   <Header />
   <slot />
+  <Footer />
 </div>

2f9ec0b9d3c12818b5081bd254803f8d218d1a0c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-17 13:07:02 +0530

    feat: added a scroll to top button that appears whenever the page is scrolled

---
+++ src/components/back-to-top.svelte
@@ -0,0 +1,49 @@
+<script>
+  export let showOnPx = 100;
+  let hidden = true;
+
+  const goToTop = () => {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const scrollContainer = () => {
+    return document.documentElement || document.body;
+  };
+
+  const handleOnScroll = () => {
+    if (!scrollContainer()) {
+      return;
+    }
+
+    if (scrollContainer().scrollTop > showOnPx) {
+      hidden = false;
+    } else {
+      hidden = true;
+    }
+  };
+</script>
+
+<svelte:window on:scroll={handleOnScroll} />
+
+<button
+  on:click={goToTop}
+  class={`fixed right-8 bottom-8 bg-accent rounded-full p-2 hover:opacity-60 transition duration-400 ${
+    hidden ? "opacity-0" : "opacity-100"
+  }`}
+  ><svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M7 11l5-5m0 0l5 5m-5-5v12"
+    />
+  </svg></button
+>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,11 @@
   import WipBanner from "../components/wip-banner.svelte";
   import Header from "../components/header.svelte";
   import Footer from "../components/footer.svelte";
+  import BackToTop from "../components/back-to-top.svelte";
 </script>
 
 <div>
+  <BackToTop />
   <WipBanner />
   <Header />
   <slot />

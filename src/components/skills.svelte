d15f44c8cf0fab36630feae4839dc52aab1fa7fd
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-20 15:14:07 +0530

    style(skills): add a hover transition to the skills bubbles

--- src/components/skills.svelte
+++ src/components/skills.svelte
@@ -5,11 +5,16 @@
 
 <div class="flex flex-wrap justify-center items-center mx-12 mt-32">
   {#each skillsData as skill}
-    <a href={skill.url}>
-      <Icon
-        iconName={skill.name}
-        className="h-16 w-16 md:h-24 md:w-24 mx-4 my-4 text-white"
-      />
+    <a
+      href={skill.url}
+      class="group flex items-center justify-between p-4 md:p-6 bg-secondary rounded-full mx-4 my-4 h-28 w-28 transition-[width] duration-200 ease-in-out hover:w-64 hover:h-32 mb-4 hover:mb-0"
+    >
+      <Icon iconName={skill.name} className="h-16 w-16 text-white" />
+      <p
+        class="opacity-0 mr-4 text-xl text-accent hidden group-hover:opacity-100 group-hover:inline-block"
+      >
+        {skill.name}
+      </p>
     </a>
   {/each}
 </div>

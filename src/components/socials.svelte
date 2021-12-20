67c7e8ce1c180ac9fda07dc814f6f6098113161f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-20 15:32:48 +0530

    feat: made the social links open in a new tab

--- src/components/socials.svelte
+++ src/components/socials.svelte
@@ -9,7 +9,11 @@
 
 <div class={`flex ${classNames}`}>
   {#each socialsData as social}
-    <a href={social.url} class={`group flex p-2 text-accent ${iconClassNames}`}>
+    <a
+      href={social.url}
+      target="_blank"
+      class={`group flex p-2 text-accent ${iconClassNames}`}
+    >
       <Icon iconName={social.name.toLowerCase()} className="h-6 w-6" />
       {#if showText}
         <p

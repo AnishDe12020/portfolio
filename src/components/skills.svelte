2b14eb7360f2bd7984dede1aa5219789b451bd7f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-20 14:42:22 +0530

    feat: clicking the link redirets you to the appropiate website

--- src/components/skills.svelte
+++ src/components/skills.svelte
@@ -5,11 +5,11 @@
 
 <div class="flex flex-wrap justify-center items-center mx-12 mt-32">
   {#each skillsData as skill}
-    <div>
+    <a href={skill.url}>
       <Icon
         iconName={skill.name}
         className="h-16 w-16 md:h-24 md:w-24 mx-4 my-4 text-white"
       />
-    </div>
+    </a>
   {/each}
 </div>

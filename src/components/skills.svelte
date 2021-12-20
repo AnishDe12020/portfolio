5389e98ac133804fdb26d814894955007d7e6097
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-20 20:07:16 +0530

    feat: add skills heading

--- src/components/skills.svelte
+++ src/components/skills.svelte
@@ -3,19 +3,26 @@
   export let skillsData;
 </script>
 
-<div class="flex flex-wrap justify-center items-center mx-12 mt-32">
-  {#each skillsData as skill}
-    <a
-      href={skill.url}
-      target="_blank"
-      class="group flex items-center justify-center hover:justify-between p-4 md:p-6 bg-secondary rounded-full mx-4 my-4 h-28 w-28 transition-[width] duration-200 ease-in-out hover:w-64 hover:h-32 mb-4 hover:mb-0"
-    >
-      <Icon iconName={skill.name} className="h-16 w-16 text-white" />
-      <p
-        class="opacity-0 mr-4 text-xl text-accent hidden group-hover:opacity-100 group-hover:inline-block"
+<div class="mx-12 mt-32">
+  <h2
+    class="text-accent text-5xl md:text-6xl font-bold mb-8 text-center lg:text-left mx-24 md:mx-32 lg:mx-48"
+  >
+    Skills
+  </h2>
+  <div class="flex flex-wrap justify-center items-center">
+    {#each skillsData as skill}
+      <a
+        href={skill.url}
+        target="_blank"
+        class="group flex items-center justify-center hover:justify-between p-4 md:p-6 bg-secondary rounded-full mx-4 my-4 h-28 w-28 transition-[width] duration-200 ease-in-out hover:w-64 hover:h-32 mb-4 hover:mb-0"
       >
-        {skill.name}
-      </p>
-    </a>
-  {/each}
+        <Icon iconName={skill.name} className="h-16 w-16 text-white" />
+        <p
+          class="opacity-0 mr-4 text-xl text-accent hidden group-hover:opacity-100 group-hover:inline-block"
+        >
+          {skill.name}
+        </p>
+      </a>
+    {/each}
+  </div>
 </div>

d02e5c0c021c315b8d0254d33f3ecf5b44405f01
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-20 14:33:01 +0530

    style: positioned skills well

--- src/components/skills.svelte
+++ src/components/skills.svelte
@@ -3,10 +3,13 @@
   export let skillsData;
 </script>
 
-<div class="flex flex-wrap">
+<div class="flex flex-wrap justify-center items-center mx-12 mt-32">
   {#each skillsData as skill}
     <div>
-      <Icon iconName={skill.name} className="h-6 w-6 text-white" />
+      <Icon
+        iconName={skill.name}
+        className="h-16 w-16 md:h-24 md:w-24 mx-4 my-4 text-white"
+      />
     </div>
   {/each}
 </div>

8892f61c8cccf673a3b197f905e8c69113e975ba
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-22 15:41:11 +0530

    feat: added a simple contact form

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -5,11 +5,18 @@
 </script>
 
 <SectionHeading id="contact">Contact</SectionHeading>
-<div class="mx-4 md:mx-24 lg:mx-32 xl:mx-40">
+<div
+  class="mx-4 md:mx-24 lg:mx-32 xl:mx-40 flex flex-col lg:flex-row justify-between"
+>
   <p class="text-white text-2xl text-center mx-4 lg:text-left">
     You can reach out to me on my email address <a
       class="text-accent hover:opacity-60"
       href={`mailto:${emailAddress}`}>{emailAddress}</a
     >
   </p>
+  <form>
+    <input type="email" placeholder="john.doe@example.com" id="email" />
+    <input type="text" placeholder="Subject" id="subject" />
+    <textarea placeholder="Your Message" id="message" />
+  </form>
 </div>

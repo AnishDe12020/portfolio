69c833e63ea3773744e77a46bf2f49417b7c9c68
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-22 15:31:34 +0530

    style: contact section email address

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -6,7 +6,7 @@
 
 <SectionHeading id="contact">Contact</SectionHeading>
 <div class="mx-4 md:mx-24 lg:mx-32 xl:mx-40">
-  <p class="text-white">
+  <p class="text-white text-2xl text-center mx-4 lg:text-left">
     You can reach out to me on my email address <a
       class="text-accent hover:opacity-60"
       href={`mailto:${emailAddress}`}>{emailAddress}</a

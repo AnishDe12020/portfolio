9db78a14145010c0abe0da47a5f3b5a3ecebec0d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-23 11:46:00 +0530

    style(contact): adjusted margins and made them responsive

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -6,7 +6,7 @@
 
 <SectionHeading id="contact">Contact</SectionHeading>
 <div
-  class="mx-4 md:mx-24 lg:mx-32 xl:mx-40 flex flex-col lg:flex-row justify-center items-center lg:justify-between items-start"
+  class="mx-4 md:mx-24 lg:mx-32 xl:mx-40 flex flex-col lg:flex-row justify-center items-center lg:justify-between lg:items-start"
 >
   <p class="text-white text-2xl text-center mx-4 lg:text-left">
     You can reach out to me on my email address <a
@@ -15,9 +15,9 @@
     >
   </p>
   <form
-    class="flex flex-col justify-center lg:justify-start items-center lg:items-start"
+    class="flex flex-col justify-center lg:justify-start items-center lg:items-start space-y-8 mt-8 lg:-mt-8"
   >
-    <div class="flex flex-col mt-8">
+    <div class="flex flex-col">
       <label for="email" class="text-white mb-4">Email</label>
       <input
         type="email"
@@ -26,7 +26,7 @@
         class="rounded-lg bg-secondary text-accent border-accent border-2 px-4 py-2"
       />
     </div>
-    <div class="flex flex-col mt-8">
+    <div class="flex flex-col">
       <label for="subject" class="text-white mb-4">Subject</label>
       <input
         type="text"
@@ -34,7 +34,7 @@
         class="rounded-lg bg-secondary text-accent border-accent border-2 px-4 py-2"
       />
     </div>
-    <div class="flex flex-col mt-8">
+    <div class="flex flex-col">
       <label for="msessage" class="text-white mb-4">Message</label>
       <textarea
         id="message"

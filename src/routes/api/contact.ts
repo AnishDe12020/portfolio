eff50e31ca9e1837c9c52b8d86a348a991f059a5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-23 14:55:51 +0530

    feat: added some error handling to the deta action

--- src/routes/api/contact.ts
+++ src/routes/api/contact.ts
@@ -3,15 +3,16 @@ import db from "$lib/deta";
 import type { Request } from "@sveltejs/kit";
 
 export const post = async (request: Request) => {
-  console.log(request.body);
-  const body = JSON.parse(request.body as string);
-  const email: string = body.email;
-  const subject: string = body.subject;
-  const message: string = body.message;
+  try {
+    const body = JSON.parse(request.body as string);
+    const email: string = body.email;
+    const subject: string = body.subject;
+    const message: string = body.message;
 
-  const response = await db.put({ email, subject, message });
-
-  console.log(response);
-
-  return { body: { response } };
+    const response = await db.put({ email, subject, message });
+    return { body: { response } };
+  } catch (error) {
+    console.error(error);
+    return { body: { error } };
+  }
 };

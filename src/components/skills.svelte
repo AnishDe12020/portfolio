2263df91e51aa28361b1f70ba6ed86c73973335f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-04 14:54:31 +0530

    refactor(skills): removed extra div

--- src/components/skills.svelte
+++ src/components/skills.svelte
@@ -5,16 +5,15 @@
 </script>
 
 <SectionHeading id="skills">Skills</SectionHeading>
-<div class="mx-2 md:mx-12">
-  <div class="flex flex-wrap justify-center items-center">
-    {#each skillsData as skill}
-      <a
-        href={skill.url}
-        target="_blank"
-        class="flex items-center justify-center bg-secondary rounded-full h-28 w-28 transition duration-400 ease-in-out my-6 mx-6 hover:shadow-accent hover:shadow-md"
-      >
-        <Icon iconName={skill.name} className="h-16 w-16 text-white" />
-      </a>
-    {/each}
-  </div>
+
+<div class="flex flex-wrap justify-center items-center mx-2 md:mx-12">
+  {#each skillsData as skill}
+    <a
+      href={skill.url}
+      target="_blank"
+      class="flex items-center justify-center bg-secondary rounded-full h-28 w-28 transition duration-400 ease-in-out my-6 mx-6 hover:shadow-accent hover:shadow-md"
+    >
+      <Icon iconName={skill.name} className="h-16 w-16 text-white" />
+    </a>
+  {/each}
 </div>

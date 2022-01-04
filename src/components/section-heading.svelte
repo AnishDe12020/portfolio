76e363483dbaf8c1584ec12f20371c7858c70fe0
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-04 15:04:21 +0530

    style(section-heading): switched to fade down animation on scroll

--- src/components/section-heading.svelte
+++ src/components/section-heading.svelte
@@ -5,7 +5,7 @@
 <h2
   {id}
   class="text-accent text-5xl md:text-6xl font-bold text-center lg:text-left mx-8 md:mx-24 lg:mx-32 xl:mx-40 my-16"
-  data-aos="fade-right"
+  data-aos="fade-down"
 >
   <slot />
 </h2>

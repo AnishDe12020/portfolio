4bfbcfecffb242b65761ad121ff05dbfa61c16fe
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-04 20:07:32 +0530

    style(section-heading): added a larger top margin

--- src/components/section-heading.svelte
+++ src/components/section-heading.svelte
@@ -4,7 +4,7 @@
 
 <h2
   {id}
-  class="text-accent text-5xl md:text-6xl font-bold text-center lg:text-left mx-8 md:mx-24 lg:mx-32 xl:mx-40 my-16"
+  class="text-accent text-5xl md:text-6xl font-bold text-center lg:text-left mx-8 md:mx-24 lg:mx-32 xl:mx-40 mb-16 mt-32 md:mt-48"
 >
   <slot />
 </h2>

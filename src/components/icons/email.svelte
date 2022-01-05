5e9906379c87d5a4da6a8d31a0d93bdf6b7d02e1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-05 17:50:58 +0530

    chore: changed twitter and email logos

--- src/components/icons/email.svelte
+++ src/components/icons/email.svelte
@@ -4,16 +4,12 @@
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="currentColor"
-  stroke-width="2"
-  stroke-linecap="round"
-  stroke-linejoin="round"
+  viewBox="0 0 20 20"
+  fill="currentColor"
   class={className}
-  ><path
-    d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
-  /><polyline points="22,6 12,13 2,6" /></svg
 >
+  <path
+    d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
+  />
+  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+</svg>

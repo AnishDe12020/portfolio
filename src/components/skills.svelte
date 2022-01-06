a925f903f20681718273ca92a3db2551a86e607a
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-01-06 19:24:12 +0530

    feat(skills): added description of skill to tooltip

--- src/components/skills.svelte
+++ src/components/skills.svelte
@@ -9,23 +9,28 @@
 
   <div class="flex flex-wrap justify-center items-center mx-2 md:mx-12">
     {#each skillsData as skill}
-      <a
-        href={skill.url}
-        aria-label={skill.name}
-        target="_blank"
-        class="flex items-center justify-center bg-secondary rounded-full h-28 w-28 transition duration-200 ease-in-out my-6 mx-6 hover:shadow-accent hover:shadow-md"
-        data-tooltip-target={`skill-tooltop-${skill.name}`}
-      >
-        <Icon iconName={skill.name} className="h-16 w-16 text-white" />
-      </a>
+      <div class="relative">
+        <a
+          href={skill.url}
+          aria-label={skill.name}
+          target="_blank"
+          class="flex items-center justify-center bg-secondary rounded-full h-28 w-28 transition duration-200 ease-in-out my-6 mx-6 hover:shadow-accent hover:shadow-md"
+          data-tooltip-target={`skill-tooltop-${skill.name}`}
+          data-tooltip-placement="right"
+        >
+          <Icon iconName={skill.name} className="h-16 w-16 text-white" />
+        </a>
 
-      <div
-        id={`skill-tooltop-${skill.name}`}
-        role="tooltip"
-        class="inline-block absolute invisible z-10 py-2 px-3  bg-secondary rounded-lg shadow-sm opacity-0 transition duration-200 tooltip"
-        data-popper-arrow
-      >
-        <span class="text-sm text-gray-100">{skill.name}</span>
+        <div
+          id={`skill-tooltop-${skill.name}`}
+          role="tooltip"
+          class="inline-block absolute invisible z-10 py-2 px-3 bg-secondary rounded-lg shadow-sm opacity-0 transition duration-200 tooltip w-48"
+        >
+          <span class="text-md text-gray-100">{skill.name}</span>
+          <hr class="text-accent my-2" />
+          <p class="text-sm text-gray-300">{skill.description}</p>
+          <div class="tooltip-arrow" data-popper-arrow />
+        </div>
       </div>
     {/each}
   </div>

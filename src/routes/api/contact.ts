62453f58446e6eeee93dd77d541cfca748b9cdeb
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-01 14:29:39 +0530

    feat(contact-api): add notiger for form responses

--- src/routes/api/contact.ts
+++ src/routes/api/contact.ts
@@ -1,6 +1,9 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import db from "$lib/deta";
 import type { Request } from "@sveltejs/kit";
+import axios from "axios";
 
 export const post = async (request: Request) => {
   try {
@@ -11,6 +14,14 @@ export const post = async (request: Request) => {
     const date = new Date().toString();
 
     const response = await db.put({ email, subject, message, date });
+    const { data, error } = await axios.post(
+      `https://www.notiger.xyz/api/events?streamId=${
+        import.meta.env.VITE_NOTIGER_STREAM_ID
+      }`,
+      { email, subject, message, date },
+      { headers: { "x-api-key": import.meta.env.VITE_NOTIGER_API_KEY } }
+    );
+    console.log(data, error);
 
     return { body: { response } };
   } catch (error) {

144338c1c8dbd82b36bb573545450f2c918ee11c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-12 16:53:39 +0530

    chore: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#010101",
+        accent: "#f3f3f3",
+      },
+    },
+  },
+  plugins: [],
+};

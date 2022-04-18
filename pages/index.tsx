4a8712b996a885b44d0fc348ea40547dfec0255a
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-18 20:13:17 +0530

    chore: add a transition with framer motion to the text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from "next";
+import { motion } from "framer-motion";
 
 const HomePage: NextPage = () => {
   return (
     <main className="flex items-center justify-center flex-col h-screen overflow-y-hidden">
-      <h1 className="text-9xl">Hey, I am Anish De</h1>
+      <motion.h1
+        initial={{ y: -50, opacity: 0.6 }}
+        transition={{
+          duration: 0.2,
+        }}
+        animate={{ y: 0, opacity: 1 }}
+        className="text-9xl"
+      >
+        Hey, I am Anish De
+      </motion.h1>
     </main>
   );
 };

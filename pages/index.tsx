b6b7d70de4dfcad7c7e627d9134c84f038d4eb3c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-18 18:51:54 +0530

    chore: add a text with syncopate font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
-  return <h1>Home page</h1>;
+  return (
+    <main className="flex items-center justify-center flex-col h-screen overflow-y-hidden">
+      <h1 className="text-9xl">Hey, I am Anish De</h1>
+    </main>
+  );
 };
 
 export default HomePage;

a32af3c9273fcf0da46f39707264c80cff96649b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-20 18:44:35 +0530

    chore: remove syncopate font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import "@fontsource/syncopate";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

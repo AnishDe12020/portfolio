0963d59408b56b74f1bb0367626e70d54a830f40
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-21 20:35:12 +0530

    style(kbar): change search bar margin to padding to prevent layout shift

--- components/KProvider/KProvider.tsx
+++ components/KProvider/KProvider.tsx
@@ -40,12 +40,13 @@ const KProvider = ({ children }: KProviderProps): JSX.Element => {
           <KBarPositioner className="z-30 bg-secondary/40 backdrop-filter backdrop-blur-md">
             {/* @ts-ignore */}
             <KBarAnimator className="rounded-xl mx-auto overflow-hidden w-[32rem] bg-secondary/80 shadow-2xl backdrop-filter backdrop-blur-lg px-4 border-[1px] border-tertiary">
-              <div className="flex justify-between my-4 items-center ml-1">
+              <div className="flex justify-between items-center ml-1 py-4">
                 <span>
                   <MagnifyingGlassIcon className="h-6 w-6 mr-2 text-gray-100" />
                 </span>
                 <KBarSearch className="w-full border-b border-gray-300 py-2 border-none bg-transparent text-gray-100 rounded-md outline-none" />
               </div>
+
               <KResults />
               <div className="h-4" />
             </KBarAnimator>

9226d4e91cee78b46df9c049ffe09709b488bca1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-21 13:35:09 +0530

    style(kbar): style the kbar

--- components/KProvider/KProvider.tsx
+++ components/KProvider/KProvider.tsx
@@ -19,14 +19,24 @@ const KProvider = ({ children }: KProviderProps): JSX.Element => {
       name: "GitHub",
       perform: () => console.log("open github"),
     },
+    {
+      id: "open-twitter",
+      name: "Twitter",
+      perform: () => console.log("open twitter"),
+    },
+    {
+      id: "open-source",
+      name: "Source",
+      perform: () => console.log("open source"),
+    },
   ];
 
   return (
     <KBarProvider actions={actions}>
       <KBarPortal>
-        <KBarPositioner>
-          <KBarAnimator>
-            <KBarSearch />
+        <KBarPositioner className="z-30 bg-primary/80 backdrop-filter backdrop-blur-md">
+          <KBarAnimator className="rounded-xl bg-primary mx-auto overflow-hidden w-[32rem] border-gray-500">
+            <KBarSearch className="w-full border-b border-gray-300 py-2 border-none bg-primary text-gray-100 rounded-md outline-none" />
             <KResults />
           </KBarAnimator>
         </KBarPositioner>
--- components/KProvider/KResults.tsx
+++ components/KProvider/KResults.tsx
@@ -11,8 +11,37 @@ const KResults = () => {
         typeof item === "string" ? (
           <div>{item}</div>
         ) : (
-          <div className={cx(active ? "bg-gray-700" : "bg-gray-900")}>
-            {item.name}
+          <div
+            className={cx(
+              "px-3 py-1 transition-colors flex items-center justify-between cursor-pointer",
+              active ? "bg-gray-700" : "bg-transparent"
+            )}
+          >
+            <div className="flex items-center">
+              {item.icon && item.icon}
+              {item.parent && (
+                <>
+                  <span>
+                    {
+                      item.ancestors.find(
+                        ancestor => ancestor.id === item.parent
+                      )?.name
+                    }
+                  </span>
+                  <span>&gt;</span>
+                </>
+              )}
+              <span>{item.name}</span>
+            </div>
+            {item.shortcut?.length && (
+              <div className="flex items-center justify-center space-x-2">
+                {item.shortcut.map(shortcut => (
+                  <kbd key={shortcut} className="bg-gray-500">
+                    {shortcut}
+                  </kbd>
+                ))}
+              </div>
+            )}
           </div>
         )
       }

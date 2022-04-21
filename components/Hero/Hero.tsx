0255896a28d474e0e36f15d6e42025f3f23b14b5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-21 22:03:53 +0530

    chore: make a hero component

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+
+const Hero = (): JSX.Element => {
+  return (
+    <main className="flex flex-col justify-center items-center h-screen">
+      <motion.h1
+        initial={{ scaleY: 1, scaleX: 1 }}
+        animate={{ scaleY: [1.2, 1, 0.6, 1], scaleX: [0.6, 1, 1.2, 1] }}
+        className="text-5xl"
+      >
+        Anish De
+      </motion.h1>
+    </main>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,8 @@
 import type { NextPage } from "next";
-import { motion } from "framer-motion";
+import Hero from "@/components/Hero";
 
 const HomePage: NextPage = () => {
-  return (
-    <main className="flex items-center justify-center flex-col h-screen overflow-y-hidden">
-      <motion.h1
-        initial={{ y: -50, opacity: 0.6 }}
-        transition={{
-          duration: 0.2,
-        }}
-        animate={{ y: 0, opacity: 1 }}
-        className="text-9xl font-bold"
-      >
-        Hey, I am{" "}
-        <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-600 to-pink-600">
-          Anish De
-        </span>
-      </motion.h1>
-    </main>
-  );
+  return <Hero />;
 };
 
 export default HomePage;

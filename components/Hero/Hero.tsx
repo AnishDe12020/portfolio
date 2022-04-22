6f562428e20e9f0f5533646b594fc9f9334bf5f7
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-22 16:09:07 +0530

    chore(hero): use description and image

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import NextImage from "next/image";
 import { Image } from "types/graphcms";
 
 interface HeroProps {
@@ -9,14 +10,24 @@ interface HeroProps {
 
 const Hero = ({ heroText, description, image }: HeroProps): JSX.Element => {
   return (
-    <main className="flex flex-col justify-center items-center mt-32">
-      <motion.h1
-        initial={{ scaleY: 1, scaleX: 1 }}
-        animate={{ scaleY: [1.2, 1, 0.6, 1], scaleX: [0.6, 1, 1.2, 1] }}
-        className="text-5xl"
-      >
-        {heroText}
-      </motion.h1>
+    <main className="flex flex-row space-x-16 mx-16">
+      <div className="flex flex-col justify-center mt-32 space-y-4">
+        <motion.h1
+          initial={{ scaleY: 1, scaleX: 1 }}
+          animate={{ scaleY: [1.2, 1, 0.6, 1], scaleX: [0.6, 1, 1.2, 1] }}
+          className="text-5xl"
+        >
+          {heroText}
+        </motion.h1>
+        <motion.p
+          className="text-xl"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          {description}
+        </motion.p>
+      </div>
+      <NextImage src={image.url} height={image.height} width={image.width} />
     </main>
   );
 };

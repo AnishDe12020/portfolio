77c378cfb8616d01eb9c72c0c506a93e2b163cee
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-25 20:57:53 +0530

    feat: make a script to generate some data for kprovider

---
+++ scripts/generate-kprovider-data.ts
@@ -0,0 +1,26 @@
+import { GraphQLClient, gql } from "graphql-request";
+import { config } from "dotenv";
+import fs from "fs";
+
+config();
+
+const KPROVIDER_DATA_FILE = "./data/kprovider.json";
+
+const generateKProviderData = async () => {
+  const graphCMSClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
+
+  const query = gql`
+    query PageHome {
+      pages {
+        slug
+        name
+      }
+    }
+  `;
+
+  const data = await graphCMSClient.request(query);
+
+  fs.writeFileSync(KPROVIDER_DATA_FILE, JSON.stringify(data));
+};
+
+generateKProviderData();

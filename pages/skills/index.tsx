ae6d2c82093b8cfc4d3a5686c55b4c3e83f1aab8
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-26 16:05:22 +0530

    feat(skills): add a link to the skill link when it is hovered upon

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -4,6 +4,7 @@ import { gql } from "@apollo/client";
 import type { Skill } from "types/graphcms";
 import IconFactory from "@/components/Shared/Icons/IconFactory";
 import Link from "next/link";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
 
 interface SkillsPageProps {
   skills: Skill[];
@@ -13,15 +14,28 @@ const SkillsPage: NextPage<SkillsPageProps> = ({ skills }) => {
   return (
     <div className="max-w-3xl mx-auto px-8">
       {skills.map(skill => (
-        <Link key={skill.id} href={`/skills/${skill.slug}`} passHref>
-          <a className="flex space-x-4 bg-secondary p-4 rounded-lg border-tertiary border-[1px] hover:border-accent transition duration-200">
-            <IconFactory name={skill.iconName} className="h-12 w-12" />
-            <div className="flex flex-col space-y-1">
-              <h2 className="text-xl font-bold">{skill.name}</h2>
-              <p className="text-gray-300 text-sm">{skill.description}</p>
-            </div>
+        <div
+          key={skill.id}
+          className="bg-secondary p-4 rounded-lg border-tertiary border-[1px] hover:border-accent transition duration-200 group relative"
+        >
+          <Link href={`/skills/${skill.slug}`} passHref>
+            <a className="flex space-x-4 ">
+              <IconFactory name={skill.iconName} className="h-12 w-12" />
+              <div className="flex flex-col space-y-1">
+                <h2 className="text-xl font-bold">{skill.name}</h2>
+                <p className="text-gray-300 text-sm">{skill.description}</p>
+              </div>
+            </a>
+          </Link>
+          <a
+            className="opacity-0 hidden group-hover:opacity-100 group-hover:flex absolute -top-2 -right-2 rounded-full p-1.5 bg-accent text-tertiary border-[1px] border-accent hover:bg-secondary hover:text-accent transition"
+            href={skill.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ExternalLinkIcon />
           </a>
-        </Link>
+        </div>
       ))}
     </div>
   );

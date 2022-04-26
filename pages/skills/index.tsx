760cabbabc6a5e43dd09507fc71b3a492772e969
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-26 15:56:41 +0530

    style(skills): added a background and border with hover styles to skills

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -4,7 +4,6 @@ import { gql } from "@apollo/client";
 import type { Skill } from "types/graphcms";
 import IconFactory from "@/components/Shared/Icons/IconFactory";
 import Link from "next/link";
-import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
 interface SkillsPageProps {
   skills: Skill[];
@@ -14,29 +13,15 @@ const SkillsPage: NextPage<SkillsPageProps> = ({ skills }) => {
   return (
     <div className="max-w-3xl mx-auto px-8">
       {skills.map(skill => (
-        <div
-          key={skill.id}
-          className="flex flex-col justify-start items-start space-y-2"
-        >
-          <Link href={`/skills/${skill.slug}`} passHref>
-            <a className="flex space-x-4">
-              <IconFactory name={skill.iconName} className="h-12 w-12" />
-              <div className="flex flex-col space-y-1">
-                <h2 className="text-xl font-bold">{skill.name}</h2>
-                <p className="text-gray-300 text-sm">{skill.description}</p>
-              </div>
-            </a>
-          </Link>
-          <a
-            href={skill.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex justify-center items-center opacity-60 text-sm space-x-1 hover:opacity-80 transition duration-200"
-          >
-            <span>{skill.link}</span>
-            <ArrowTopRightIcon />
+        <Link key={skill.id} href={`/skills/${skill.slug}`} passHref>
+          <a className="flex space-x-4 bg-secondary p-4 rounded-lg border-tertiary border-[1px] hover:border-accent transition duration-200">
+            <IconFactory name={skill.iconName} className="h-12 w-12" />
+            <div className="flex flex-col space-y-1">
+              <h2 className="text-xl font-bold">{skill.name}</h2>
+              <p className="text-gray-300 text-sm">{skill.description}</p>
+            </div>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );

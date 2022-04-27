d1191889df7269a85f36faca6a6a8944c2a50832
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-27 15:25:09 +0530

    style(skill-page): some responsive styles and add a bg to the icon

--- pages/skills/[slug].tsx
+++ pages/skills/[slug].tsx
@@ -13,8 +13,11 @@ const SkillPage: NextPage<SkillsPageProps> = ({ skill }) => {
   console.log(skill);
   return (
     <>
-      <div className="flex space-x-8">
-        <IconFactory name={skill.iconName} className="h-16 w-16" />
+      <div className="flex space-x-8 mt-8">
+        <IconFactory
+          name={skill.iconName}
+          className="h-16 w-16 bg-tertiary p-3 rounded-lg shadow-md"
+        />
         <div className="flex flex-col space-y-2">
           <h1 className="text-2xl font-bold">{skill.name}</h1>
           <p className="text-gray-300 text-sm">{skill.description}</p>
@@ -22,7 +25,7 @@ const SkillPage: NextPage<SkillsPageProps> = ({ skill }) => {
       </div>
       <a
         href={skill.link}
-        className="mt-4 text-gray-300 flex space-x-1 items-end text-sm hover:text-gray-100 transition duration-200"
+        className="mt-4 md:mt-6 text-gray-300 flex space-x-1 items-end text-sm hover:text-gray-100 transition duration-200"
       >
         <span>{skill.link}</span> <ArrowUpRight className="h-4 w-4" />
       </a>

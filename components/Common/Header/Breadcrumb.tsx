cd1e024ec510b9f31135c685db667c438f25387c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-27 14:22:58 +0530

    fix(breadcrumb): remove unused import

--- components/Common/Header/Breadcrumb.tsx
+++ components/Common/Header/Breadcrumb.tsx
@@ -1,5 +1,4 @@
 import capitalize from "@/utils/helpers";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import BreadcrumbItem from "./BreadcrumbItem";

3b37cc21d4a2ae6f924710a72e18bf135d63a126
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-28 15:00:07 +0530

    fix(circles): changed from fixed to absolute so that it doesnt overlap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,21 +12,21 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <KProvider>
       <Header />
       <div className="max-w-3xl mx-auto px-8">
+        <Component {...pageProps} />
+        <CustomToaster />
         {isEnabled && (
-          <div className="fixed top-0 left-0 right-0">
-            <div className="fixed overflow-visible opacity-20 left-8 md:left-32 lg:left-48 top-16 -z-10">
+          <div className="absolute top-0 left-0 right-0">
+            <div className="absolute overflow-visible opacity-20 left-8 md:left-32 lg:left-48 top-16 -z-10">
               <div className="mix-blend-multiply h-[400px] w-[700px] bg-red-700 rounded-full blur-[128px] circle-1 -z-10" />
             </div>
-            <div className="fixed overflow-visible opacity-20 left-10 md:left-48 lg:left-72 top-32 -z-10">
+            <div className="absolute overflow-visible opacity-20 left-10 md:left-48 lg:left-72 top-32 -z-10">
               <div className="mix-blend-multiply h-[400px] w-[700px] bg-green-700 rounded-full blur-[128px] circle-2 -z-10" />
             </div>
-            <div className="fixed overflow-visible opacity-20 left-12 md:left-72 lg:left-96 top-64 -z-10">
+            <div className="absolute overflow-visible opacity-20 left-12 md:left-72 lg:left-96 top-64 -z-10">
               <div className="mix-blend-multiply h-[600px] w-[500px] bg-blue-700 rounded-full blur-[128px] circle-3 -z-10" />
             </div>
           </div>
         )}
-        <Component {...pageProps} />
-        <CustomToaster />
       </div>
     </KProvider>
   );

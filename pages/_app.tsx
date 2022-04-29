a59c5cbca0990547a79b9dbf638187f6ddfb7708
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-29 14:27:54 +0530

    style(circles): fix overflow, positioning and animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,25 +9,27 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const { isEnabled } = useCircles();
 
   return (
-    <KProvider>
-      <Header />
-      <div className="max-w-3xl mx-auto px-8">
-        <Component {...pageProps} />
-        <CustomToaster />
-        {isEnabled && (
-          <div className="absolute top-0 left-0 right-0">
-            <div className="absolute overflow-visible opacity-20 left-8 md:left-32 lg:left-48 top-16 -z-10">
-              <div className="mix-blend-multiply h-[400px] w-[700px] bg-red-700 rounded-full blur-[128px] circle-1 -z-10" />
+    <div className="relative min-h-screen overflow-hidden">
+      <KProvider>
+        <Header />
+        <main className="max-w-3xl mx-auto px-8 relative">
+          <Component {...pageProps} />
+          <CustomToaster />
+          {isEnabled && (
+            <div className="absolute top-0 left-0 right-0">
+              <div className="absolute overflow-visible opacity-20 left-8top-16 -z-10">
+                <div className="mix-blend-multiply h-[400px] w-[700px] bg-red-700 rounded-full blur-[128px] circle-1 -z-10" />
+              </div>
+              <div className="absolute overflow-visible opacity-20 left-10 top-32 -z-10">
+                <div className="mix-blend-multiply h-[400px] w-[700px] bg-green-700 rounded-full blur-[128px] circle-2 -z-10" />
+              </div>
+              <div className="absolute overflow-visible opacity-20 left-12 top-48 -z-10">
+                <div className="mix-blend-multiply h-[600px] w-[500px] bg-blue-700 rounded-full blur-[128px] circle-3 -z-10" />
+              </div>
             </div>
-            <div className="absolute overflow-visible opacity-20 left-10 md:left-48 lg:left-72 top-32 -z-10">
-              <div className="mix-blend-multiply h-[400px] w-[700px] bg-green-700 rounded-full blur-[128px] circle-2 -z-10" />
-            </div>
-            <div className="absolute overflow-visible opacity-20 left-12 md:left-72 lg:left-96 top-64 -z-10">
-              <div className="mix-blend-multiply h-[600px] w-[500px] bg-blue-700 rounded-full blur-[128px] circle-3 -z-10" />
-            </div>
-          </div>
-        )}
-      </div>
-    </KProvider>
+          )}
+        </main>
+      </KProvider>
+    </div>
   );
 }

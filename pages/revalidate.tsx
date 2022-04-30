06913cc9c65f526a289316865c439634c9456aa8
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-30 22:37:50 +0530

    feat(revalidate): use form for authorization

--- pages/revalidate.tsx
+++ pages/revalidate.tsx
@@ -1,24 +1,26 @@
 import axios from "axios";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const Revalidate: NextPage = () => {
-  const [loading, setLoading] = useState<boolean>(true);
   const [authorized, setAuthorized] = useState<boolean>(false);
-  const [password, setPassword] = useState<string>("");
+
+  const { register, handleSubmit } = useForm();
 
   useEffect(() => {
     if (!localStorage) {
       return;
-    } else {
-      setPassword(localStorage.getItem("password"));
     }
 
     const checkAuth = async () => {
+      const password = localStorage.getItem("password");
       const res = await axios.post("/api/check-admin-access", {
         password,
       });
       if (res.status === 200 && res.data.message === "Authorized") {
+        localStorage.setItem("password", password);
         setAuthorized(true);
       } else {
         setAuthorized(false);
@@ -26,21 +28,50 @@ const Revalidate: NextPage = () => {
     };
 
     checkAuth();
-  }, [password]);
+  }, []);
+
+  const onPasswordSubmit = async (data: any) => {
+    const password = data.password;
+    const res = await axios.post("/api/check-admin-access", {
+      password,
+    });
+    if (res.status === 200 && res.data.message === "Authorized") {
+      localStorage.setItem("password", password);
+      toast.success("Authorized");
+      setAuthorized(true);
+    } else {
+      toast.error("Wrong password");
+      setAuthorized(false);
+    }
+  };
 
   return (
     <>
       <h1>Revalidate</h1>
-      {authorized ? (
-        <p>Authorized</p>
-      ) : (
-        <input
-          className="text-accent focus:outline-none focus:ring-4 focus:ring-opacity-60 rounded-md px-4 py-2 border-secondary border-4 bg-tertiary transition duration-200 focus:border-blue-500"
-          value={password}
-          placeholder="Admin Password"
-          onChange={e => setPassword(e.target.value)}
-        />
-      )}
+      <div className="mt-8 flex flex-col">
+        {authorized ? (
+          <>
+            <button>Revalidate home</button>
+          </>
+        ) : (
+          <form
+            onSubmit={handleSubmit(onPasswordSubmit)}
+            className="flex space-x-2"
+          >
+            <input
+              className="text-accent focus:outline-none focus:ring-4 focus:ring-opacity-60 rounded-md px-4 py-2 border-secondary border-4 bg-tertiary transition duration-200 focus:border-blue-500"
+              placeholder="Admin Password"
+              {...register("password", { required: true })}
+            />
+            <button
+              type="submit"
+              className="rounded-lg bg-accent px-4 py-2 hover:opacity-60 transition duration-200 text-primary"
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
     </>
   );
 };

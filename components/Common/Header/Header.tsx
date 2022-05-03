7ce1649c6ab53f8ed02c75b79766b0d4f8682573
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-03 15:19:56 +0530

    refactor(header): move router call to header component

--- components/Common/Header/Header.tsx
+++ components/Common/Header/Header.tsx
@@ -1,14 +1,25 @@
+import { useRouter } from "next/router";
 import Breadcrumb from "./Breadcrumb";
 import NavItem from "./NavItem";
 
 const Header = (): JSX.Element => {
+  const { asPath } = useRouter();
+
   return (
     <div className="flex justify-between mx-8 my-4">
       <Breadcrumb />
-      <nav className="flex space-x-2">
-        <NavItem href="/">Home</NavItem>
-        <NavItem href="/skills">Skills</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
+      <nav>
+        <ul className="flex space-x-2">
+          <NavItem href="/" asPath={asPath}>
+            Home
+          </NavItem>
+          <NavItem href="/skills" asPath={asPath}>
+            Skills
+          </NavItem>
+          <NavItem href="/projects" asPath={asPath}>
+            Projects
+          </NavItem>
+        </ul>
       </nav>
     </div>
   );
--- components/Common/Header/NavItem.tsx
+++ components/Common/Header/NavItem.tsx
@@ -1,28 +1,28 @@
 import Link from "next/link";
 import cx from "classnames";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
 interface NavItemProps {
   href: string;
   children: ReactNode;
+  asPath: string;
 }
 
-const NavItem = ({ href, children }: NavItemProps): JSX.Element => {
-  const { asPath } = useRouter();
-
+const NavItem = ({ href, children, asPath }: NavItemProps): JSX.Element => {
   return (
-    <Link href={href} passHref>
-      <a
-        className={cx(
-          "text-gray-500 px-3 py-1 hover:bg-gray-600 hover:bg-opacity-30 rounded-lg transition duration-200 text-base",
-          asPath.split("/")[1] === href.replace("/", "") &&
-            "font-medium !text-accent bg-gray-600 bg-opacity-30 hover:bg-opacity-40"
-        )}
-      >
-        {children}
-      </a>
-    </Link>
+    <li>
+      <Link href={href} passHref>
+        <a
+          className={cx(
+            "text-gray-500 px-3 py-1 hover:bg-gray-600 hover:bg-opacity-30 rounded-lg transition duration-200 text-base",
+            asPath.split("/")[1] === href.replace("/", "") &&
+              "font-medium !text-accent bg-gray-600 bg-opacity-30 hover:bg-opacity-40"
+          )}
+        >
+          {children}
+        </a>
+      </Link>
+    </li>
   );
 };
 

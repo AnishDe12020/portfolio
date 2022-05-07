e9e1eff9a11136a15d3a71b6c44a6815e4ffda91
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-07 16:41:25 +0530

    chore(home): move from directus to static data

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,23 +1,13 @@
 import { motion } from "framer-motion";
 import NextImage from "next/image";
-import { DirectusImage } from "types/directus";
 
-interface HeroProps {
-  heroHeading: string;
-  heroText: string;
-  image: DirectusImage;
-}
+import AvararJPG from "public/static/images/avatar.jpg";
 
-const Hero = ({ heroHeading, heroText, image }: HeroProps): JSX.Element => {
+const Hero = (): JSX.Element => {
   return (
     <div className="flex flex-col space-y-8">
       <div className="relative h-32 w-32 overflow-hidden rounded-full">
-        <NextImage
-          src={image.url}
-          layout="fill"
-          placeholder="blur"
-          blurDataURL={image.previewURL}
-        />
+        <NextImage src={AvararJPG} layout="fill" placeholder="blur" />
       </div>
       <div className="flex flex-col justify-center space-y-4">
         <motion.h1
@@ -25,14 +15,14 @@ const Hero = ({ heroHeading, heroText, image }: HeroProps): JSX.Element => {
           animate={{ opacity: 1 }}
           className="text-5xl font-bold"
         >
-          {heroHeading}
+          Anish De
         </motion.h1>
         <motion.p
           className="text-xl"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
         >
-          {heroText}
+          {"// Todo"}
         </motion.p>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,13 @@
-import type { GetStaticProps, NextPage } from "next";
-import { HomePageCollection } from "types/directus";
+import type { NextPage } from "next";
 
 import Hero from "@/components/Home/Hero";
-import getAssetUrl from "@/utils/getAssetUrl";
-import getPreviewImageUrl from "@/utils/getPreviewImageURL";
 
-import directus from "lib/directus";
 
-interface HomePageProps {
-  content: HomePageCollection;
-}
-
-const HomePage: NextPage<HomePageProps> = ({ content }) => {
+const HomePage: NextPage = () => {
   return (
     <Hero
-      heroHeading={content.heroHeading}
-      heroText={content.heroText}
-      image={content.heroImage}
     />
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await directus.singleton("homePage").read({
-    fields:
-      "heroHeading, heroText, heroImage.id, heroImage.height, heroImage.width",
-  });
-
-  data.heroImage.url = getAssetUrl(data.heroImage.id);
-
-  data.heroImage.previewURL = await getPreviewImageUrl(data.heroImage.url);
-
-  console.log(data);
-  return {
-    props: { content: data },
-  };
 };
 
 export default HomePage;

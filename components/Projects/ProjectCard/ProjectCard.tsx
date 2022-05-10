477b8d43faee07e458a0db13c0340882828fb485
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-10 20:27:01 +0530

    style(project-card): add hover scale transition

--- components/Projects/ProjectCard/ProjectCard.tsx
+++ components/Projects/ProjectCard/ProjectCard.tsx
@@ -22,7 +22,7 @@ const ProjectCard = ({
   githubLink,
 }: ProjectProps): JSX.Element => {
   return (
-    <div className="flex flex-col space-y-8 rounded-lg border-[1px] border-tertiary bg-secondary p-4 transition duration-200 md:h-48 md:flex-row md:space-y-0 md:space-x-8">
+    <div className="flex flex-col space-y-8 rounded-lg border-[1px] border-tertiary bg-secondary p-4 transition duration-200 md:h-48 md:flex-row md:space-y-0 md:space-x-8 md:hover:scale-105">
       <div className="overflow-hidden rounded-lg">
         <div className="relative h-36 w-full md:h-full md:w-72">
           <NextImage

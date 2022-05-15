82ad5085e39a1627cc718c6f61718c8202521d3e
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-15 14:49:27 +0530

    style(blog-post-card): fixed hover styles and use grid positioning on blog page

--- components/Blog/BlogPostCard/BlogPostCard.tsx
+++ components/Blog/BlogPostCard/BlogPostCard.tsx
@@ -1,6 +1,5 @@
 import NextImage from "next/image";
 
-import { cleanLink, getGitHubOwnerAndRepoFromLink } from "@/utils/helpers";
 import Link from "@/components/Shared/Link";
 import { CloudinaryImage } from "contentlayer/generated";
 
@@ -20,23 +19,23 @@ const ProjectCard = ({
   readingTime,
 }: BlogPostCardProps): JSX.Element => {
   return (
-    <article className="group flex max-w-lg flex-col-reverse rounded-xl border-[1px] border-tertiary bg-secondary py-4 px-6">
-      <div className="flex-col space-y-4">
-        <Link href={`/blog/${slug}`}>
-          <h2 className="text-lg font-semibold group-hover:opacity-60">
+    <article className="flex max-w-lg flex-col-reverse rounded-xl border-[1px] border-tertiary bg-secondary py-4 px-6 transition duration-200 hover:scale-105">
+      <div>
+        <Link href={`/blog/${slug}`} className="flex-col space-y-4">
+          <h2 className="text-lg font-semibold transition duration-200 hover:opacity-60">
             {title}
           </h2>
+          <p className="text-gray-300 transition duration-200 hover:opacity-60">
+            {date} / {readingTime}
+          </p>
         </Link>
-        <p className="text-gray-300">
-          {date} / {readingTime}
-        </p>
       </div>
       <Link href={`/blog/${slug}`} className="aspect-[16/9]">
         <NextImage
           src={image.url}
           height={image.height}
           width={image.width}
-          className="rounded-lg drop-shadow-md group-hover:opacity-80"
+          className="rounded-lg drop-shadow-md transition duration-200 hover:opacity-60"
         />
       </Link>
     </article>
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -11,16 +11,18 @@ const BlogPostsPage: NextPage<BlogPostsPageProps> = ({ posts }) => {
   return (
     <>
       <h1 className="mb-8 text-2xl font-bold">Blog Posts</h1>
-      {posts.map(post => (
-        <BlogPostCard
-          key={post._id}
-          title={post.title}
-          image={post.image}
-          slug={post.slug}
-          date={post.date}
-          readingTime={post.readingTime.text}
-        />
-      ))}
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        {posts.map(post => (
+          <BlogPostCard
+            key={post._id}
+            title={post.title}
+            image={post.image}
+            slug={post.slug}
+            date={post.date}
+            readingTime={post.readingTime.text}
+          />
+        ))}
+      </div>
     </>
   );
 };

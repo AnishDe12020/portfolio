4df79f9ee8a00c4d15ea45e602cde0cba836cad1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-15 09:39:54 +0530

    feat(mdx-components): add a custom image component with rounded styles and a margin

--- components/Common/MDXComponents/MDXComponents.tsx
+++ components/Common/MDXComponents/MDXComponents.tsx
@@ -1,8 +1,27 @@
 import { MDXComponents } from "mdx/types";
 import Link from "@/components/Shared/Link";
+import { cloneElement, useRef } from "react";
+
+const CodeBlock = props => {
+  const codeRef = useRef<HTMLElement>(null);
+
+  console.log(codeRef);
+  return (
+    <div style={{ position: "relative", overflow: "auto" }}>
+      <pre {...props}>
+        {cloneElement(props.children as React.ReactElement, { ref: codeRef })}
+      </pre>
+    </div>
+  );
+};
+
+const BlogImage = props => {
+  return <img {...props} className="my-8 rounded-xl bg-secondary p-2" />;
+};
 
 const CustomMDXComponents: MDXComponents = {
   a: Link,
+  pre: CodeBlock,
+  img: BlogImage,
 };
-
 export default CustomMDXComponents;

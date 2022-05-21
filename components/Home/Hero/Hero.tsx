29f4f925089ad492029d7eb7f1044b68c7893bb2
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-21 14:34:26 +0530

    style(hero): add some entry animations

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,26 +1,47 @@
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import NextImage from "next/image";
 
 import AvatarJPG from "public/static/images/avatar.jpg";
+
+const fadeInVariant: Variants = {
+  offscreen: {
+    opacity: 0,
+    y: -20,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const Hero = (): JSX.Element => {
   return (
     <div className="flex flex-col-reverse md:flex-row md:justify-between md:space-y-0">
       <div className="mt-8 flex flex-col justify-center space-y-4 md:mt-0 md:w-2/3">
         <motion.h1
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
           className="text-5xl font-bold"
+          variants={fadeInVariant}
+          initial="offscreen"
+          animate="onscreen"
+          transition={{ delay: 0.3 }}
         >
           Anish De
         </motion.h1>
-        <motion.h2 className="text-lg font-medium text-gray-400">
+        <motion.h2
+          className="text-lg font-medium text-gray-400"
+          variants={fadeInVariant}
+          initial="offscreen"
+          animate="onscreen"
+          transition={{ delay: 0.4 }}
+        >
           10th grader • Fullstack Web Developer • Blogger • Tech Enthusiast
         </motion.h2>
         <motion.p
           className="text-md text-lg text-gray-300"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          variants={fadeInVariant}
+          initial="offscreen"
+          animate="onscreen"
+          transition={{ delay: 0.6 }}
         >
           I am a fullstack web developer, currently looking into learning new
           things (three.js, Rust, Go, Web3 and backend technologies). Whenever I
@@ -28,9 +49,13 @@ const Hero = (): JSX.Element => {
           blog posts and social media.
         </motion.p>
       </div>
-      <div className="relative h-32 w-32 overflow-hidden rounded-full">
+      <motion.div className="relative h-32 w-32 overflow-hidden rounded-full" variants={fadeInVariant}
+          initial="offscreen"
+          animate="onscreen"
+          transition={{ delay: 0.1 }}
+>
         <NextImage src={AvatarJPG} layout="fill" placeholder="blur" />
-      </div>
+      </motion.div>
     </div>
   );
 };

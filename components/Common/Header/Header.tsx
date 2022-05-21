f77e5f0992570788a7274408c6b307613198e032
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-21 17:44:14 +0530

    style(header): push the skip to content button further up

--- components/Common/Header/Header.tsx
+++ components/Common/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header = (): JSX.Element => {
     <nav className="relative my-4 flex items-center justify-between px-8">
       <a
         href="#main"
-        className="shadow-md z-index-50 absolute left-1/2 -translate-y-16 rounded-lg border-[1px] border-tertiary bg-secondary px-4 py-2 transition duration-200 focus:block focus:translate-y-0 focus:border-accent"
+        className="shadow-md z-index-50 absolute left-1/2 -translate-y-32 rounded-lg border-[1px] border-tertiary bg-secondary px-4 py-2 transition duration-200 focus:block focus:translate-y-0 focus:border-accent"
         tabIndex={100}
       >
         Skip to main content

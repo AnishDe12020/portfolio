bc744164ecdfc5a7567842cb747e323941c8ef7d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-03 18:14:22 +0530

    feat(index): show blog posts on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from "next";
 
 import Hero from "@/components/Home/Hero";
 import Projects from "@/components/Home/Projects";
+import BlogPosts from "@/components/Home/BlogPosts";
 
 const HomePage: NextPage = () => {
   return (
     <>
       <Hero />
       <Projects />
+      <BlogPosts />
     </>
   );
 };

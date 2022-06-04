71c79364536610fe351c95a49e1398e96453b372
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-04 15:26:25 +0530

    chore(projects-page): remove animations

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,16 +1,11 @@
 import { GetStaticProps, NextPage } from "next";
-import { motion } from "framer-motion";
 
 import ProjectCard from "@/components/Projects/ProjectCard";
 
 import { allProjects, Project } from "contentlayer/generated";
-import { fadeInVariant } from "variants";
 import Link from "@/components/Shared/Link";
 import { ArrowRight } from "react-feather";
 import getPreviewImageUrl from "@/utils/getPreviewImageURL";
-
-const MotionProjectCard = motion(ProjectCard);
-const MotionLink = motion(Link);
 
 export interface ProjectWithPlaceholderImage extends Project {
   placeholderImage: string;
@@ -27,17 +22,15 @@ const ProjectsPage: NextPage<ProjectsPageProps> = ({
 
   return (
     <>
-      <motion.h1
-        variants={fadeInVariant}
-        initial="offscreen"
-        animate="onscreen"
+      <h1
+    
         className="mb-8 text-2xl font-bold"
       >
         Projects
-      </motion.h1>
+      </h1>
       <div className="flex-col space-y-8">
         {allProjectsWithPlaceholderImages.map((project, index) => (
-          <MotionProjectCard
+          <ProjectCard
             key={project._id}
             name={project.name}
             description={project.description}
@@ -46,25 +39,17 @@ const ProjectsPage: NextPage<ProjectsPageProps> = ({
             slug={project.slug}
             image={project.image}
             placeholderImage={project.placeholderImage}
-            variants={fadeInVariant}
-            initial="offscreen"
-            animate="onscreen"
-            transition={{ delay: 0.2 + 0.3 * index, duration: 0.3 }}
           />
         ))}
       </div>
-      <MotionLink
+      <Link
         href="https://github.com/stars/AnishDe12020/lists/projects"
-        className="will-change-opacity group mt-8 flex flex-row items-center justify-start space-x-2 text-xl font-medium will-change-transform"
+        className="group mt-8 flex flex-row items-center justify-start space-x-2 text-xl font-medium"
         noExternalLinkIcon
-        variants={fadeInVariant}
-        initial="offscreen"
-        animate="onscreen"
-        transition={{ delay: 0.3 * allProjects.length, duration: 0.2 }}
       >
         <span>View more on GitHub</span>
         <ArrowRight className="h-4 w-4 transition duration-200 group-hover:translate-x-1" />
-      </MotionLink>
+      </Link>
     </>
   );
 };

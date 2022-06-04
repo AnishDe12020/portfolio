21f2e348f0fa19da54a7e98e1de785326aae81a5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-04 18:05:29 +0530

    chore(footer): add link to source

--- components/Common/Footer/Footer.tsx
+++ components/Common/Footer/Footer.tsx
@@ -3,10 +3,13 @@ import Socials from "@/components/Shared/Socials";
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="mx-auto mb-16 flex max-w-3xl flex-col items-center space-x-2">
+    <footer className="mx-auto mb-16 flex max-w-3xl flex-col items-center space-y-4">
       <Socials small className="mb-8" />
       <p>
         Made by <Link href="https://twitter.com/AnishDe12020">Anish De</Link>
+      </p>
+      <p>
+        This portfolio is open source and the source code can be found <Link href="https://github.com/AnishDe12020/portfolio">on GitHub</Link>
       </p>
     </footer>
   );

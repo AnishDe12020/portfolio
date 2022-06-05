4a864b307264d6eef155740df0336d7350b77f14
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-05 16:04:43 +0530

    style(footer): add some padding and centre all text

--- components/Common/Footer/Footer.tsx
+++ components/Common/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Socials from "@/components/Shared/Socials";
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="mx-auto mb-16 flex max-w-3xl flex-col items-center space-y-4">
+    <footer className="mx-auto mb-16 flex max-w-3xl flex-col items-center space-y-4 px-8 text-center">
       <Socials small className="mb-8" />
       <p>
         Made by <Link href="https://twitter.com/AnishDe12020">Anish De</Link>

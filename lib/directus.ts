a71a045e69c6a15276ed6b082c109df22866cdb3
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-07-07 17:58:50 +0530

    refactor: remove unused packages and some unused code

--- lib/directus.ts
+++
@@ -1,6 +0,0 @@
-import { Directus } from "@directus/sdk";
-import { Collections } from "types/directus";
-
-const directus = new Directus<Collections>(process.env.DIRECTUS_URL);
-
-export default directus;
--- utils/getAssetUrl.ts
+++
@@ -1,7 +0,0 @@
-import { ID } from "@directus/sdk";
-
-const getAssetUrl = (id: ID) => {
-  return `${process.env.DIRECTUS_URL}/assets/${id}`;
-};
-
-export default getAssetUrl;
